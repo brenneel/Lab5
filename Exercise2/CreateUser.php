fe51afd9d6f2c5dab4edbc4a2ac5d79ffaae9115
Ethan Brenner <ethanlbrenner@gmail.com>
2019-10-27 21:12:56 -0500

    back end now reports correctly to user

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -1,5 +1,7 @@
 <?php
 $istheSame = false;
+$input = $_POST['inputBox'];
+
 $mysqli = new mysqli("mysql.eecs.ku.edu", "ethanlbrenner", "brenner3280", "ethanlbrenner");
 
 /* check connection */
@@ -15,16 +17,19 @@ if ($result = $mysqli->query($query)) {
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
         if($row["user_id"] == $_POST["inputBox"]){
-            printf("comparison says they are the same");
+            printf("user already exists");
             $istheSame = true;
         }
 
-        printf ("%s\n", $row["user_id"]);
+        //printf ("%s\n", $row["user_id"]);
     }
     if($istheSame == false){
-     $query = "INSERT INTO Users (user_id) VALUES ('hello')";
-     $mysqli->query($query);
+     $query2 = "INSERT INTO Users (user_id) VALUES ('$input')";
+     
+     if($mysqli->query($query2)){
         printf("successfully added\n");
+     }
+        
     }
 
     /* free result set */
