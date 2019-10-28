ba1519471c7d68732d0cd3f0bf256bacde6021c8
Ethan Brenner <ethanlbrenner@gmail.com>
2019-10-27 21:38:01 -0500

    added boilerplate php from exercise2

--- Exercise3/CreatePosts.php
+++ Exercise3/CreatePosts.php
@@ -1,5 +1,42 @@
 <?php
+$istheSame = false;
+$inputUser = $_POST['inputUser'];
+$inputPost = $_POST['inputPosts'];
 
-echo("worked");
+$mysqli = new mysqli("mysql.eecs.ku.edu", "ethanlbrenner", "brenner3280", "ethanlbrenner");
 
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "SELECT use_id FROM Users ORDER by user_id";
+
+if ($result = $mysqli->query($query)) {
+
+    /* fetch associative array */
+    while ($row = $result->fetch_assoc()) {
+        if($row["user_id"] == $_POST["inputBox"]){
+            printf("user already exists");
+            $istheSame = true;
+        }
+
+        //printf ("%s\n", $row["user_id"]);
+    }
+    if($istheSame == false){
+     $query2 = "INSERT INTO Users (user_id) VALUES ('$input')";
+     
+     if($mysqli->query($query2)){
+        printf("successfully added\n");
+     }
+        
+    }
+
+    /* free result set */
+    $result->free();
+}
+
+/* close connection */
+$mysqli->close();
 ?>
